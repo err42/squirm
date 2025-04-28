2a1e5e9cbf341c82f9b22c4bfd015368883be635
Sean <sean@nodal.digital>
2025-04-27 23:08:00 -0400

    did some reorg, threads reporting and running but need to fix wall time

--- squirm.py
+++ squirm.py
@@ -1,23 +1,61 @@
+#!/usr/bin/env python3
+import os
 import sys
+import logging
 import threading
 import schedule
+import datetime
 import time
 import subprocess
 from functools import partial
+from pathlib import Path
 
+## init globals
+count = 0
+pwd = Path.cwd()
+
+## add logging
+logging.basicConfig(
+    level=logging.INFO,
+    filename="squirm.log",
+    filemode="w",
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%H:%M:%S",
+)
 
 ## execute jobs
 def run_threaded(job_func):
-    job_thread = threading.Thread(target=job_func)
-    start = datetime.datetime.now()
+    job_path = Path(job_func)
+    print(
+         "Starting",
+         job_func,
+         "running on thread %s" % threading.current_thread(),
+     )
+    job_thread = threading.Thread(target=job_path)
+    start_time = time.time()
     job_thread.start()
-    job_thread.join()
-    end = datetime.datetime.now()
-    return end - start
+    job_thread.join()  # Wait for the thread to finish
+    end_time = time.time()
+    diffr = end_time - start_time
+    logging.info("run_threaded ran")
+    print(diffr)
 
+def run_regular(dep_func):
+    tmp_path = str(pwd) + "/" + str(dep_func)
+    dep_path = Path(tmp_path)
+    print(
+         "Starting",
+         dep_path,
+         "running on thread %s" % threading.current_thread(),
+     )
+    process = subprocess.Popen(dep_path, shell=False, stdout=subprocess.PIPE)
+    process.wait()
+    logging.info("run_regular ran")
 
 ## confirm file exists and is pythonic
 def validate_job(job):
+    global count
+    count += 1
     try:
         result = subprocess.run(
             ["black", "--check", "-q", job],
@@ -31,17 +69,16 @@ def validate_job(job):
 
 ## get jobs from user
 def main():
-    count = 0
+    global count
     inputfile = sys.argv[1]
+    wall_time = '-'
     with open(inputfile, "r") as task_list:
         for lines in task_list.read().splitlines():
             curr_line = lines.split()
             name = str(curr_line[0])
             time_est = curr_line[1]
             job_req = str(curr_line[2:])
-            wall_time = "-"
-            count += 1
-            ## throws exception if no deps given, so ignored.
+            ## throws exception if commit flag not thrown when checked, so caught & ignored.
             try:
                 committed = str(sys.argv[2])
             except IndexError:
@@ -59,23 +96,71 @@ def main():
             ## validate tasks, consider depends
             retval = validate_job(name)
             if retval == 0:
+                isvalid = "YES"
+                if str(job_req) == "[]":
+                    job_req = '-'
+                print(
+                     "Order",
+                     count,
+                     "Task Name:",
+                     name,
+                     "Is Valid?",
+                     isvalid,
+                     "Est.Time",
+                     time_est,
+                     "Wall.Time",
+                     wall_time,
+                     "Dependencies",
+                     str(job_req),
+                 )
                 if job_req is not None:
                     ## check deps validity
                     for dep in strippedvals.split():
                         depval = validate_job(dep)
                         if depval != 0:
+                            isvalid = "DEP. FAIL"
                             print(
-                                "Task",
+                                "Order",
+                                count,
+                                "Task dependency",
                                 dep,
                                 "NOT VALID and therefore will not be submitted. Received Return Code:",
                                 depval,
                             )
-                            isvalid = "DEP. FAIL"
-                            break
                         else:
-                            isvalid = "PASS"
+                            isvalid = "YES"
+                            if committed != 0:
+                                print(
+                                     "--commit flag caught & task(s) validated. Now running dependent tasks",
+                                     name,
+                                     ":",
+                                 )
+                                run_regular(name)
+                                print(
+                                     "--commit flag caught & task(s) validated. Now running dependent tasks",
+                                     dep.split(),
+                                     ":",
+                                 )
+                                run_regular(dep)
+
+
+                               
+                            else:
+                                print(
+                                    "Order",
+                                    count,
+                                    "Dependency Name:",
+                                    dep,
+                                    "Is Valid?",
+                                    isvalid,
+                                    "Est.Time",
+                                    time_est,
+                                    "Wall.Time",
+                                    wall_time,
+                                )
                 else:
-                    isvalid = "PASS"
+                    isvalid = "YES"
+                    count += 1
                     print(
                         "Order",
                         count,
@@ -83,44 +168,18 @@ def main():
                         name,
                         "Is Valid?",
                         isvalid,
-                        "Est. Time",
+                        "Est.Time",
                         time_est,
-                        "Wall Time",
+                        "Wall.Time",
                         wall_time,
                         "Dependencies",
                         str(job_req),
                     )
 
-                    ## run jobs if user committed
-                    if committed != 0:
-                        print(
-                            "--commit flag caught & task(s) validated. Now running #",
-                            count,
-                            ":",
-                        )
-                        print(
-                            "Starting",
-                            name,
-                            "running on thread %s" % threading.current_thread(),
-                        )
-                        schedule.every(1).seconds.do(partial(run_threaded, name))
-                        print(
-                            "Order",
-                            count,
-                            "Task Name:",
-                            name,
-                            "Is Valid?",
-                            isvalid,
-                            "Est. Time",
-                            time_est,
-                            "Wall Time",
-                            wall_time,
-                            "Dependencies",
-                            str(job_req),
-                        )
 
             else:
-                isvalid = "PRI. FAIL"
+                isvalid = "PRI.FAIL"
+                count += 1
                 print(
                     "Order",
                     count,
@@ -128,15 +187,15 @@ def main():
                     name,
                     "Is Valid?",
                     isvalid,
-                    "Est. Time",
+                    "Est.Time",
                     time_est,
-                    "Wall Time",
+                    "Wall.Time",
                     wall_time,
                     "Dependencies",
                     str(job_req),
                 )
 
-    schedule.run_pending()
+#    schedule.run_pending()
 
 
 if __name__ == "__main__":
